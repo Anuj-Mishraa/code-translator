1e498fceead06a8691712bd0ad145db7f78e5658
Anuj Mishra <anujmishra282003@gmail.com>
2023-08-15 18:44:22 +0530

    nmk

--- app.py
+++ app.py
@@ -1,34 +1,47 @@
-import sagemaker
-import boto3
-from sagemaker.huggingface import HuggingFaceModel
+import gradio as gr
+import requests
+import os
 
-try:
-	role = sagemaker.get_execution_role()
-except ValueError:
-	iam = boto3.client('iam')
-	role = iam.get_role(RoleName='sagemaker_execution_role')['Role']['Arn']
+API_URL = "https://api-inference.huggingface.co/models/openai-gpt"
+API_TOKEN = os.environ.get("API_TOKEN")
 
-# Hub Model configuration. https://huggingface.co/models
-hub = {
-	'HF_MODEL_ID':'microsoft/speecht5_tts',
-	'HF_TASK':'text-to-speech'
-}
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-# create Hugging Face Model Class
-huggingface_model = HuggingFaceModel(
-	transformers_version='4.26.0',
-	pytorch_version='1.13.1',
-	py_version='py39',
-	env=hub,
-	role=role, 
+# Function to translate code using the Hugging Face model API
+# Function to translate code using the Hugging Face model API
+# Function to translate code using the Hugging Face model API
+def translate_code(input_text, source_lang, target_lang):
+    payload = {
+        "inputs": f"convert the below {source_lang} code to {target_lang} code: {input_text}"
+    }
+    
+    response = requests.post(API_URL, headers=headers, json=payload)
+    response_data = response.json()  # Store the entire response for inspection
+    print("API Response:", response_data)  # Print the response for inspection
+    
+    # Extract the translated code from the response
+    translated_code = "No translation available"  # Default value
+    
+    if response_data:
+        if isinstance(response_data, list) and len(response_data) > 0:
+            translated_code = response_data[0].get("generated_text", "").strip()
+    
+    return translated_code
+
+
+
+# Interface for the Gradio app
+iface = gr.Interface(
+    fn=translate_code,
+    inputs=[
+        gr.inputs.Textbox(label="Enter code to translate"),
+        gr.inputs.Textbox(label="Source Language (e.g., English)"),
+        gr.inputs.Textbox(label="Target Language (e.g., German)")
+    ],
+    outputs=gr.outputs.Textbox(label="Translated Code"),
+    title="Code Translator",
+    description="Translate code snippets between programming languages"
 )
 
-# deploy model to SageMaker Inference
-predictor = huggingface_model.deploy(
-	initial_instance_count=1, # number of instances
-	instance_type='ml.m5.xlarge' # ec2 instance type
-)
-
-predictor.predict({
-	"inputs": "The answer to the universe is 42",
-})
+# Launch the Gradio app
+iface.launch()
